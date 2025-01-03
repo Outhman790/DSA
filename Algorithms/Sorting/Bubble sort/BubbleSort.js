b9a19cfdfe3b1bd4c85a9258f064fd4e2f408c2e
Outhman Moumou <tangerino2011@gmail.com>
2025-01-03 17:33:07 +0100

    Made some enhancements

--- Algorithms/Sorting/Bubble sort/BubbleSort.js
+++ Algorithms/Sorting/Bubble sort/BubbleSort.js
@@ -1,25 +1,21 @@
-const bubbleSortAscending = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - i; j++) {
-      if (array[j + 1] < array[j]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+// The bubbleSort function is a simple sorting algorithm that sorts an array in ascending or descending order by repeatedly swapping adjacent elements if they are in the wrong order. It takes two parameters: arr (the array to be sorted) and ascending (a boolean flag indicating whether to sort in ascending or descending order). The function returns the sorted array.
+// Time complexity: O(n^2)
+const bubbleSort = (arr, ascending = true) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++)
+      if (!ascending) {
+        if (arr[j] < arr[j + 1]) {
+          let temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      } else {
+        if (arr[j] > arr[j + 1]) {
+          let temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
       }
-    }
   }
-  return array;
-};
-
-const bubbleSortDescending = function (array) {
-  for (let i = array.length; i > 0; i--) {
-    for (let j = array.length; j > 0; j--) {
-      if (array[j - 1] < array[j]) {
-        let temp = array[j];
-        array[j] = array[j - 1];
-        array[j - 1] = temp;
-      }
-    }
-  }
-  return array;
+  return arr;
 };
