421784fe6f5518983a8bfa8d8e80734fdd062dfa
Outhman Moumou <tangerino2011@gmail.com>
2025-01-07 21:31:30 +0100

    Selection sort in js

---
+++ Algorithms/Sorting/Selection Sort/SelectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  }
+  return arr;
+}
+console.log(selectionSort([2, 1, 5, 4, 3]));
