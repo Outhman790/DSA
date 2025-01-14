550f6fc2bc27a0fbf7563cddc1569eb06babe983
Outhman Moumou <tangerino2011@gmail.com>
2025-01-14 18:27:57 +0100

    Created a single linked list with the push method

---
+++ Data Structures/Linked lists/Single Linked List/SingleLinkedList.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+}
+
+const ll = new LinkedList(1);
+ll.push(2);
+ll.push(3);
+console.log(ll);
