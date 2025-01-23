318650f3a9faed36c59787bc1eead15d208a3a14
Outhman Moumou <tangerino2011@gmail.com>
2025-01-23 23:41:40 +0100

    shift method

--- Data Structures/Linked lists/Single Linked List/SingleLinkedList.js
+++ Data Structures/Linked lists/Single Linked List/SingleLinkedList.js
@@ -38,7 +38,17 @@ class LinkedList {
     this.length--;
     return temp;
   }
-
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.tail === temp) {
+      this.tail = null;
+    }
+    return temp;
+  }
   unshift(value) {
     const newNode = new Node(value);
     newNode.next = this.head;
